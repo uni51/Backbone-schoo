6b57a32d17cc375ab8a08c61887d62bd04ea64f5
uni51 <uni51@users.noreply.github.com>
2018-09-29 01:14:50 +0900

    JSエンジニア養成 ch07 メモの削除

--- backbone-training/notepad/js/note_list_item.js
+++ backbone-training/notepad/js/note_list_item.js
@@ -5,10 +5,25 @@ App.NoteListItemView = Backbone.View.extend({
 
   tagName: 'tr',
 
+  initialize: function() {
+    // モデルのdestroyイベントを監視してBackbone.Viewのremove()メソッドを呼び出す
+    this.listenTo(this.model, 'destroy', this.remove);
+  },
+
+  // [Delete]ボタンを監視して、onclickDelete()メソッドを呼び出す
+  events: {
+    'click .js-delete': 'onclickDelete'
+  },
+
   render: function() {
     var template = $('#noteListItemView-template').html();
     var html = _.template(template, this.model.toJSON());
     this.$el.html(html);
     return this;
+  },
+
+  onclickDelete: function() {
+    // モデルを削除する
+    this.model.destroy();
   }
 })
